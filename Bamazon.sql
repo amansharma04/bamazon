8b9323e686306bac5d7215ad4223e23bb7553e7c
Aman <amansharma965@gmail.com>
2018-08-26 12:53:13 -0700

    initial commit

---
+++ Bamazon.sql
@@ -0,0 +1,76 @@
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `bamazon` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `bamazon`;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `item_id` int(30) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(30) NOT NULL,
+  `department_name` varchar(30) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `stock_quantity` int(30) DEFAULT NULL,
+  PRIMARY KEY (`item_id`),
+  UNIQUE KEY `item_id` (`item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Garbanzo Beans','Canned Goods',3.29,349),(2,'5 lb. Bag of Rice','Pantry Essentials',12.99,694),(3,'Walnuts','Self-stable Goods',7.99,22),(4,'Sesame Seeds','Spices',4.75,10),(5,'Frozen Peas and Carrots','Frozen Vegetables',6.90,300),(6,'Frozen Cauliflower','Frozen Vegetables',4.30,207),(7,'Black Beans','Canned Goods',2.99,457),(8,'Cayenne Pepper','Spices',5.69,32),(9,'Penne Pasta','Self-stable Goods',4.99,345),(10,'2 lb. Bag of Rice','Pantry Essentials',7.69,389);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-15 22:24:58
+
+-- department table
+
+
+CREATE TABLE departments (
+	department_id int(30) not null AUTO_INCREMENT,
+	department_name varchar(30) DEFAULT NULL,
+	over_head_costs decimal(30,2) DEFAULT NULL,
+	primary key(department_id)
+);
+
+INSERT INTO departments(department_name, over_head_costs) VALUES ("Canned Goods", 3453), ("Pantry Essentials", 4322), ("Frozen Vegetables", 12200), ("Spices", 9000);
+
+INSERT INTO departments(department_name, over_head_costs) VALUES ("Shelf-Stable Goods", 34889);
+
+-- added column to products after creation and added values
+
+ALTER TABLE products ADD product_sales DECIMAL(30,2) DEFAULT 0;
+
+-- add grocery ids to the product table
+
+ALTER TABLE products Add department_id integer;
+
+ALTER TABLE products Add FOREIGN KEY (department_id) REFERENCES departments(department_id);
+
+update products SET department_id = 1 where department_name = "Canned Goods";
+
+update products SET department_id = 2 where department_name = "Pantry Essentials";
+
+update products SET department_id = 3 where department_name = "Frozen Vegetables";
+
+update products SET department_id = 4 where department_name = "Spices";
+
+update products SET department_id = 5 where department_name = "Shelf-Stable Goods";
---
+++ bamazonSupervisor.js
@@ -0,0 +1,100 @@
+ar mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    // no port set = default port 3306
+
+    user: "root",
+
+    password: "",
+    database: "bamazon"
+});
+
+// test connection
+connection.connect(function(error){
+    if (error) throw error;
+    // console.log("connected to " + connection.threadId);
+});
+
+function start() {
+
+    inquirer.prompt([
+    {
+        name: "menuItem",
+        type: "list",
+        message: "Please select what you would like to do.",
+        choices: ["View Product Sales By Department", "Create New Department"]
+    }
+    ]).then(function(answer){
+
+        var menuOption = answer.menuItem;
+
+        switch (menuOption) {
+            case "View Product Sales By Department":
+                sales();
+                break;
+            case "Create New Department":
+                create();
+                break;
+        }
+    });
+};
+
+start();
+
+function sales() {
+
+    connection.query("SELECT departments.*, SUM(products.product_sales) AS total_product_sales, (SUM(products.product_sales) - over_head_costs) AS total_profit FROM `departments` JOIN products ON departments.department_id=products.department_id GROUP BY departments.department_id", function(error, response){
+
+        var table = new Table({
+            head: ['department_id', 'department_name', 'over_head_costs', 'product_sales', 'total_profit']
+          , colWidths: [14, 20, 14, 14, 14]
+            });
+
+        response.forEach(function(response){
+
+            var id = response.department_id;
+            var name = response.department_name;
+            var costs = response.over_head_costs;
+            var sales = response.total_product_sales;
+            var total = response.total_profit;
+
+            table.push(
+                [id, name, costs, sales, total]
+            );
+        });
+
+        console.log(table.toString());
+
+    connection.end();
+    })
+}
+
+function create() {
+
+    inquirer.prompt([
+        {
+            name: "newDepartmentName",
+            type: "input",
+            message: "Please enter the name of your new department."
+        },
+        {
+            name: "newOverheadCosts",
+            type: "input",
+            message: "Please enter the overhead costs for this department."
+        }
+    ]).then(function(answer){
+
+        var department =  answer.newDepartmentName;
+        var costs = answer.newOverheadCosts;
+
+        connection.query("INSERT INTO departments(department_name, over_head_costs) VALUES ('" + department + "'," + costs + ")", function(error, response){
+            if (error) throw error;
+
+            console.log("You've added the department named " + department + " to your records, with the overhead costs of " + costs + ".");
+        })
+    connection.end();
+    });
+}
