2b94b11607dc19befd3932670e47d8d5bf42eca8
Aman <amansharma965@gmail.com>
2018-08-26 14:06:28 -0700

    almost

--- Bamazon.sql
+++ Bamazon.sql
@@ -1,22 +1,21 @@
-DROP DATABASE IF EXISTS Bamazon;
-
-CREATE DATABASE Bamazon;
-
-USE bamazon_DB;
-
+CREATE DATABASE bamazon;
+USE bamazon;
 CREATE TABLE products (
-	item_id int auto_increment not null,
-    product_name varchar(100) not null,
-    department_name varchar(100) not null,
-    price int not null,
-    stock_quantity int(5),
-    primary key (item_id)
+ item_id INT NOT NULL AUTO_INCREMENT,
+ product_name VARCHAR(30) NOT NULL,
+ department_name VARCHAR(30),
+ price FLOAT(10,2),
+ stock_quantity INT NOT NULL,
+ PRIMARY KEY (item_id)
 );
-
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUE ('Sofa', 'Home',  299.99, 3), 
-('Shirt Men', 'Clothing',12.99, 20), 
-('Shirt Women','Clothing',  14.99, 20), 
-('Oxfords', 'Shoes', 44.99, 8),
-('Chucks','Shoes',  89.99, 10),
-('Derby','Shoes', 399.99, 15);
+VALUES ("AA Batteries 20-pack", "Household Supplies", 10.00, 23),
+("Cordless Drill", "Tools and Home Improvement", 30.00, 5),
+("LED Video Projector", "Electronics", 400.00, 3),
+("Memory Foam Pillow 2-pack", "Home and Kitchen", 40.00, 12),
+("Dry Erase Marker Set", "Office Products", 6.24, 39),
+("Magnetic Dry Erase Board", "Office Products", 31.90, 8),
+("Electronic Scale", "Health and Personal Care", 18.99, 54),
+("Stainless Steel Water Bottle", "Home and Kitchen", 13.99, 47),
+("Combination Padlock", "Tools and Home Improvement", 5.01, 113),
+("Cell Phone Charging Cable", "Cell Phone Accessories", 9.99, 148);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,101 +1,47 @@
-//add variables that show which node packages will be required
-var mysql = require('mysql');
-var inquirer = require('inquirer');
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
-// create the connection information for the sql database
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Password!",
-    database: "Bamazon.sql"
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Friday$19",
+  database: "bamazon"
 });
 
-//Asks user what they'd like to buy
-function beginPrompt() {
+connection.connect(function (err) {
+  if (err) throw err;
+  postItems();
+});
 
+function postItems() {
+  connection.query("SELECT * FROM products", function (err, queryResult) {
+    if (err) throw err;
+    var products = [];
+    queryResult.forEach((e, i) => products[i] = e.item_id + ": " + e.product_name);
     inquirer.prompt([
+      {
+        name: "product",
+        type: "list",
+        message: "Select the product you would like to buy.",
+        choices: products
+      }
+    ]).then(function (answer) {
+      var num = answer.product.split(":")[0] - 1;
+      inquirer.prompt([
         {
-            type: 'input',
-            name: 'item_id',
-            message: 'Hello, please enter the Item ID for the item you will be purchasing',
-        },
-        {
-            type: 'input',
-            name: 'quantity',
-            message: 'How many of this item would you like to buy?',
+          name: "answer",
+          type: "input",
+          message: "The price of this item is $" + queryResult[num].price + "\n  There are " + queryResult[num].stock_quantity + " of this item available. How many would you like to buy?",
 
         }
-    ]).then(function (input) {
-
-        var item = input.item_id;
-        var quantity = input.quantity;
-        var startCnstrct = 'SELECT * FROM products WHERE ?';
-
-        connection.query(startCnstrct, { item_id: item }, function (err, data) {
-            if (err) throw err;
-
-            if (data.length === 0) {
-                console.log('The item id you entered is not in our system.');
-                showItems();
-
-            } else {
-                var productData = data[0];
-                if (quantity <= productData.stock_quantity) {
-                    console.log('Congratulations, the product you requested is in stock! Placing order!');
-                    var updatestartCnstrct = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
-                    // Update stock
-                    connection.query(updatestartCnstrct, function (err, data) {
-                        if (err) throw err;
-
-                        console.log('Thank you for your order! Your order total is $' + productData.price * quantity);
-                        console.log('We appreciate your business. Please come again.');
-                        console.log("\n****************************************************************\n");
-
-                        // disconnect
-                        connection.end();
-                    })
-                } else {
-                    console.log('Oh no! We are out of this item. Please change your order, or contact us at 1-800-bmzn');
-                    console.log("\n****************************************************************\n");
-
-                    showItems();
-                }
-            }
-        })
-    })
+      ])
+    });
+  });
 }
-
-// this function will show current stock of products
-function showItems() {
-
-    startCnstrct = 'SELECT * FROM products';
-
-    connection.query(startCnstrct, function (err, data) {
-        if (err) throw err;
-
-        console.log('\nHere is what we have in stock:');
-
-        var inputDisp = '';
-        for (var i = 0; i < data.length; i++) {
-            inputDisp = '';
-            inputDisp += 'Item ID: ' + data[i].item_id + '  *  ';
-            inputDisp += 'Product Name: ' + data[i].product_name + '  *  ';
-            inputDisp += 'Department: ' + data[i].department_name + '  *  ';
-            inputDisp += 'Price: $' + data[i].price + '\n';
-
-            console.log(inputDisp);
-        }
-
-        console.log("***************************************************************\n");
-
-        beginPrompt();
-    })
-}
-
-function startApp() {
-    showItems();
-}
-
-//Begin app
-startApp();
